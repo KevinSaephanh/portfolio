45812334407a5b0df943f0ac8c5bb93a7771c3ab
Kevin <Kevin.Saephanh@gmail.com>
2020-06-11 20:36:07 -0500

    Animated loading bar

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,18 +4,26 @@ import Skills from "./Skills.json";
 import "./About.css";
 
 const About = () => {
+    const [skills, setSkills] = React.useState(Skills.skills);
+
+    React.useEffect(() => {
+        setTimeout(() => {
+            skills.forEach((s) => {
+                animateLoad(s, s.proficiency, 0);
+            });
+        }, 200);
+    }, []);
+
     const getSkills = () => {
         return (
             <ul className="skills">
-                {Skills.skills.map((skill, key) => {
+                {skills.map((skill, key) => {
                     return (
                         <li className="skill" key={key}>
                             <h3>{skill.tech}</h3>
-                            <div
-                                className="proficiency"
-                                style={{ width: skill.proficiency * 5 }}
-                            />
-                            <h3>{skill.proficiency}%</h3>
+                            <div id={`${skill.tech}`} className="proficiency">
+                                {skill.proficiency}%
+                            </div>
                         </li>
                     );
                 })}
@@ -23,14 +31,32 @@ const About = () => {
         );
     };
 
+    const animateLoad = (skill, goal, iterator) => {
+        if (iterator < goal) {
+            const skillStyle = document.getElementById(`${skill.tech}`).style;
+            iterator++;
+
+            setTimeout(() => {
+                skillStyle.background = `linear-gradient(90deg, rgb(33, 203, 209) ${iterator}%, black ${
+                    100 - iterator
+                }%`;
+                animateLoad(skill, goal, iterator);
+            }, 10);
+        }
+    };
+
     return (
         <div className="about">
             <img src={Me} alt="" />
             <p>
-                I'm a Full Stack Developer from California. My work mainly
-                involves concentration on full stack apps, but I like to
-                occasionally dabble in pure front-end projects.
+                I'm a Full Stack Developer from California. I began my
+                programming journey in 2016 in college starting with C++. In
+                2019, my interest in programming led me to the world of web apps
+                and full stack development. Ever since, I've been passionate
+                about creating fun and sometimes useful web apps that showcase
+                my interests and artistic sense.
             </p>
+            <h2>Languages / Technologies</h2>
             {getSkills()}
         </div>
     );

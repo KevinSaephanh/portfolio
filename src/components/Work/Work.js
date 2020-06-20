23c86171310e3e9fc6950b342eac96b3d99a9c65
Kevin <Kevin.Saephanh@gmail.com>
2020-06-20 00:04:17 -0500

    Updated projects json and projects UI

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -3,19 +3,39 @@ import Projects from "./Projects.json";
 import "./Work.css";
 
 const Work = () => {
-    const { projs } = Projects;
+    const [projects, setProjects] = React.useState(Projects.projs);
 
     const getProjects = () => {
         return (
             <ul className="projects">
-                {projs.map((proj, key) => {
+                {projects.map((proj, key) => {
                     return (
-                        <li className="project" key={key} title={proj.title}>
+                        <li
+                            className="project"
+                            key={key}
+                            title={`${proj.title} - click to visit`}
+                        >
                             <a href={proj.link}>
                                 <img src={proj.pic} alt="" />
-                                <h3>{proj.title}</h3>
-                                <h3>{proj.stack}</h3>
+                                <div className="project-info">
+                                    <h3>{proj.title}</h3>
+                                    {getStack(proj)}
+                                </div>
                             </a>
+                        </li>
+                    );
+                })}
+            </ul>
+        );
+    };
+
+    const getStack = (project) => {
+        return (
+            <ul className="stack">
+                {project.stack.map((s, key) => {
+                    return (
+                        <li className="stack-item" key={key}>
+                            {s.name}
                         </li>
                     );
                 })}

31410ca7b327c50d19fc7960ebadcab93fb4a43c
Kevin <kevin.saephanh@gmail.com>
2021-08-19 20:29:04 -0500

    Added react-spring for animations

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,30 @@
 import React from "react";
-import Typing from "react-typing-animation";
+import { useTransition, animated } from "react-spring";
+
+const items = [
+  { text: "Hello, I'm Kevin Saephanh", y: -50, delay: 400 },
+  { text: "A Full Stack Developer", y: -20, delay: 800 },
+  { text: "I build web applications for a living", y: 0, delay: 1200 },
+];
 
 const Home = () => {
+  const transition = useTransition(items, {
+    from: { y: 500, opacity: 0 },
+    enter: (item) => async (next) => {
+      await next({ y: item.y, opacity: 1, delay: item.delay });
+    },
+    leave: {},
+  });
+
   return (
-    <div className="content">
-      <Typing speed={80}>
-        <h1>Hello, I'm Kevin Saephanh</h1>
-        <h2>A Full Stack Developer</h2>
-      </Typing>
+    <div className="home content">
+      {transition((style, item) =>
+        item ? (
+          <animated.div style={style}>
+            <h1>{item.text}</h1>
+          </animated.div>
+        ) : null
+      )}
     </div>
   );
 };

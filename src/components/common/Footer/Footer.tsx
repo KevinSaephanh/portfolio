29ccc2263b80b1bce924abd566d7aa6565d2e338
Kevin <kevin.saephanh@gmail.com>
2022-12-25 11:32:46 -0600

    Add react-icons for footer

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -1,45 +1,31 @@
-import clsx from 'clsx';
 import React from 'react';
-
-const socials = [
-  {
-    icon: 'fa fa-linkedin',
-    title: 'LinkedIn',
-    link: 'https://www.linkedin.com/in/kevin-saephanh',
-  },
-  {
-    icon: 'fa fa-square-github',
-    title: 'Github',
-    link: 'https://github.com/KevinSaephanh',
-  },
-  {
-    icon: 'fa fa-twitter',
-    title: 'Twitter',
-    link: 'https://twitter.com/KevCoolio93',
-  },
-  {
-    icon: 'fa-brands fa-discord',
-    title: 'Discord',
-    link: 'https://www.discord.com/users/360657658270973956',
-  },
-];
+import { BsLinkedin, BsGithub, BsTwitter, BsDiscord } from 'react-icons/bs';
 
 export const Footer: React.FC = () => {
   return (
-    <footer className='text-center w-full pb-4'>
-      <ul className='flex items-center justify-center space-x-2 mb-2 w-5/6 md:w-full'>
-        {socials.map((social, key) => (
-          <li className='text-2xl' key={key}>
-            <a href={social.link}>
-              <i
-                className={clsx('hover:text-teal-300', `${social.icon}`)}
-                title={social.title}
-                aria-hidden='true'
-              />
-            </a>
-          </li>
-        ))}
-      </ul>
+    <footer className='text-center pb-4'>
+      <div className='flex items-center justify-center text-2xl space-x-2 mb-2'>
+        <a
+          href='https://www.linkedin.com/in/kevin-saephanh'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <BsLinkedin />
+        </a>
+        <a href='https://github.com/KevinSaephanh' target='_blank' rel='noopener noreferrer'>
+          <BsGithub />
+        </a>
+        <a href='https://twitter.com/KevCoolio93' target='_blank' rel='noopener noreferrer'>
+          <BsTwitter />
+        </a>
+        <a
+          href='https://www.discord.com/users/360657658270973956'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <BsDiscord />
+        </a>
+      </div>
       <span className='text-md'>© 2022 Kevin Saephanh</span>
     </footer>
   );

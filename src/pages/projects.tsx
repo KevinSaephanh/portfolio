8c6e1ab85d449f37a2f14fae3e9a4197dd207e55
Kevin <kevin.saephanh@gmail.com>
2023-01-25 12:08:46 -0600

    Update tech stack style for projects to grid

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -6,6 +6,7 @@ import { TbWorld } from 'react-icons/tb';
 
 const projects = [
   {
+    id: 1,
     path: '/pathfinder.png',
     title: 'Data Generator',
     desc: 'An app that allows users to create mock data (in JSON format) and modules for environment variable types (env.d.ts)',
@@ -14,6 +15,7 @@ const projects = [
     site: 'https://inquisitive-entremet-11acdd.netlify.app/',
   },
   {
+    id: 2,
     path: '/pathfinder.png',
     title: 'Pathfinder',
     desc: 'A visualization of maze generation and pathfinding algorithms. A maze is first generated in real time. Then the maze will be traversed, marking each visited cell. Finally, the optimal path to the escape route will be highlighted.',
@@ -22,6 +24,7 @@ const projects = [
     site: 'https://kevinsaephanh.github.io/Pathfinder/',
   },
   {
+    id: 3,
     path: '/sort_visualizer.png',
     title: 'Sort Visualizer',
     desc: 'This app also produces algorithm visualizations. A graph is displayed containing a random array of bars of varying length. After starting, bars begin moving left and right to imitate how the sorting algorithm works. Once sorting is completed, all bars will be highlighted.',
@@ -30,6 +33,7 @@ const projects = [
     site: 'https://kevinsaephanh.github.io/Sort_Visualizer/',
   },
   {
+    id: 4,
     path: '/sort_visualizer.png',
     title: 'Placeholder',
     desc: 'This app also produces algorithm visualizations. A graph is displayed containing a random array of bars of varying length. After starting, bars begin moving left and right to imitate how the sorting algorithm works. Once sorting is completed, all bars will be highlighted.',
@@ -37,10 +41,19 @@ const projects = [
     code: 'https://github.com/KevinSaephanh/Sort_Visualizer',
     site: 'https://kevinsaephanh.github.io/Sort_Visualizer/',
   },
+  {
+    id: 5,
+    path: '/sort_visualizer.png',
+    title: 'AlgoProToday',
+    desc: 'Technical interview prep platform. Level up your skills by tackling popular industry algorithms/data structures questions.\nNote: this was my very first full-stack application.',
+    stack: ['React', 'Bootstrap', 'JavaScript', 'Redux', 'Node', 'Express', 'MongoDB'],
+    code: 'https://github.com/KevinSaephanh/algoprotoday',
+    site: '',
+  },
 ];
 
 const Projects: NextPage = () => {
-  const [active, setActive] = React.useState(1);
+  const [active, setActive] = React.useState(projects[1]);
 
   return (
     <div className='md:w-2/3 lg:w-2/4 md:mx-auto themed-border'>
@@ -51,9 +64,9 @@ const Projects: NextPage = () => {
               <li
                 key={key}
                 className={`whitespace-nowrap cursor-pointer p-2 md:p-1 rounded hover:text-teal-300 ${
-                  active === key ? 'bg-slate-500' : ''
+                  active.id === key + 1 ? 'bg-slate-500' : ''
                 }`}
-                onClick={() => setActive(key)}
+                onClick={() => setActive(projects[key])}
               >
                 {p.title}
               </li>
@@ -63,10 +76,10 @@ const Projects: NextPage = () => {
         <article className='flex-center flex-col md:flex-row p-4'>
           <div className='md:w-1/3'>
             asdf
-            {/* <Image src={projects[active].path} alt='' fill={true} className='themed-border' /> */}
+            {/* <Image src={active.path} alt='' fill={true} className='themed-border' /> */}
           </div>
-          <ul className='flex flex-row md:flex-col md:w-2/4'>
-            {projects[active].stack.map((s) => (
+          <ul className='flex flex-row flex-wrap md:grid-list-cols md:w-2/4 h-full justify-evenly'>
+            {active.stack.map((s) => (
               <li className='mr-3 md:mr-0'>{s}</li>
             ))}
           </ul>
@@ -74,20 +87,17 @@ const Projects: NextPage = () => {
       </section>
       <section className='flex-center flex-col mb-4 md:mb-8 p-4 rounded border-t-2 border-black dark:border-slate-100'>
         <article className='flex-center px-4 md:mx-auto mb-4'>
-          <span className='text-center'>{projects[active].desc}</span>
+          <span className='text-center whitespace-pre-line'>{active.desc}</span>
         </article>
         <article className='flex-center'>
-          <a
-            className='text-2xl mr-4'
-            href={projects[active].code}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
+          <a className='text-2xl mr-4' href={active.code} target='_blank' rel='noopener noreferrer'>
             <BsGithub />
           </a>
           <a
-            className='text-3xl'
-            href={projects[active].site}
+            className={`text-3xl ${
+              active.site.length <= 0 ? 'pointer-events-none opacity-50' : ''
+            }`}
+            href={active.site}
             target='_blank'
             rel='noopener noreferrer'
           >

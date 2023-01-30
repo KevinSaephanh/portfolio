e0ad6df374302316babaf8a0a3f30ccf04587f46
Kevin <kevin.saephanh@gmail.com>
2023-01-29 23:16:50 -0600

    Add threejs and assets

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -7,7 +7,7 @@ import { TbWorld } from 'react-icons/tb';
 const projects = [
   {
     id: 1,
-    path: '/pathfinder.png',
+    path: '/assets/pathfinder.png',
     title: 'Data Generator',
     desc: 'Create up to 1000 records of mock data for your application, using common properties of entities such as email, names, and encrypted text.\nIn addition, upload .env files to create modules for environment variable types to make them type-safe.',
     stack: ['React', 'Typescript', 'Tailwind'],
@@ -16,7 +16,7 @@ const projects = [
   },
   {
     id: 2,
-    path: '/pathfinder.png',
+    path: '/assets/pathfinder.png',
     title: 'Pathfinder',
     desc: `A visualization of maze generation and pathfinding algorithms. Here's how it works:\n\u2022 A maze is first generated in real time using a maze generation algorithm.\n\u2022 Then the maze will be traversed via a pathfinding algorithm, marking each visited cell.\n\u2022 Finally, the optimal path to the escape route will be highlighted.`,
     stack: ['React', 'JavaScript', 'Bootstrap'],
@@ -25,7 +25,7 @@ const projects = [
   },
   {
     id: 3,
-    path: '/sort_visualizer.png',
+    path: '/assets/sort_visualizer.png',
     title: 'Sort Visualizer',
     desc: 'A visualization of sorting algorithms.\n\u2022 A graph is displayed containing a random array of bars of varying length.\n\u2022 After starting, bars begin moving left and right to imitate how the sorting algorithm works.\n\u2022 Once sorting is completed, all bars will be highlighted.',
     stack: ['React', 'TypeScript', 'Bootstrap'],
@@ -34,21 +34,21 @@ const projects = [
   },
   {
     id: 4,
-    path: '/sort_visualizer.png',
-    title: 'Placeholder',
-    desc: 'This app also produces algorithm visualizations. A graph is displayed containing a random array of bars of varying length. After starting, bars begin moving left and right to imitate how the sorting algorithm works. Once sorting is completed, all bars will be highlighted.',
-    stack: ['React', 'TypeScript', 'Bootstrap'],
-    code: 'https://github.com/KevinSaephanh/Sort_Visualizer',
-    site: 'https://kevinsaephanh.github.io/Sort_Visualizer/',
-  },
-  {
-    id: 5,
-    path: '/sort_visualizer.png',
+    path: '/assets/sort_visualizer.png',
     title: 'AlgoProToday',
     desc: 'Technical interview prep platform. Level up your skills by tackling popular industry questions regarding data structures and algortihms.\nNote: this was my very first full-stack application.',
     stack: ['React', 'Bootstrap', 'JavaScript', 'Redux', 'Node', 'Express', 'MongoDB'],
     code: 'https://github.com/KevinSaephanh/algoprotoday',
     site: '',
+  },
+  {
+    id: 5,
+    path: '/assets/sort_visualizer.png',
+    title: 'Placeholder',
+    desc: 'This app also produces algorithm visualizations. A graph is displayed containing a random array of bars of varying length. After starting, bars begin moving left and right to imitate how the sorting algorithm works. Once sorting is completed, all bars will be highlighted.',
+    stack: ['React', 'TypeScript', 'Bootstrap'],
+    code: 'https://github.com/KevinSaephanh/Sort_Visualizer',
+    site: 'https://kevinsaephanh.github.io/Sort_Visualizer/',
   },
 ];
 

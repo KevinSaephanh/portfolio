dd255ff5bfc7a420a6c0788cdc2534b33e0be893
Kevin Saephanh <Kevin.Saephanh@gmail.com>
2023-12-26 11:57:22 -0600

    Update Scene component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import { Scene } from '@/components/Canvas/Scene';
+import { Terrain } from '@/components/Canvas/Terrain';
 import * as React from 'react';
 
 export default function Home() {
   return (
     <div className='h-full w-full'>
-      <Scene />
+      <Scene autoRotate={true}>
+        <Terrain />
+      </Scene>
     </div>
   );
 }
